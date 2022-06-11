6970395b51bf8391d7aed09fd8d66951b3eeeab0
Ashley Fitzsimons <ashleyfitzsimons_x@hotmail.com>
2022-06-11 22:10:02 +0000

    fix db

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -119,7 +119,7 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse('DATABASE_URL')
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
@@ -128,7 +128,6 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
 
 
 # Password validation
